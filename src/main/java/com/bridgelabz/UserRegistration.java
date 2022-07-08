6565040f030f8dce43449a89888eb64aebda37ca
swatikanishu <nishuswatika@gmail.com>
2022-07-08 21:33:22 +0530

    Added: Second name validation added

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -5,9 +5,14 @@ public class UserRegistration {
         return Pattern.matches("^[A-Z][a-z]{2,}", validFirstName);
     }
 
+    public static boolean givenLastName_WhenValid_ShouldReturnTrue(String validLastName) {
+        return Pattern.matches("^[A-Z][a-z]{2,}", validLastName);
+    }
     public static void main(String[] args) {
                 boolean firstName = givenFirstName_WhenValid_ShouldReturnTrue("Riya");
                 System.out.println("first name is valid" + firstName);
+        boolean lastName = givenFirstName_WhenValid_ShouldReturnTrue("Meera");
+        System.out.println("lastName is valid" + lastName);
     }
 
 }
