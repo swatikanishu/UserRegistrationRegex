bf0b32c419cbbc82a5e51ac46c6e6fa8b88a123d
swatikanishu <nishuswatika@gmail.com>
2022-07-08 21:06:06 +0530

    Added: Welcome message is added

---
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println(" welcome to UserRegistration program");
+    }
+}
