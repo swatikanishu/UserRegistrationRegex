60f6302c808b0e0b2f22c028842916446cd3d37f
swatikanishu <nishuswatika@gmail.com>
2022-07-08 21:37:04 +0530

    Added: Email validation added

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -8,11 +8,16 @@ public class UserRegistration {
     public static boolean givenLastName_WhenValid_ShouldReturnTrue(String validLastName) {
         return Pattern.matches("^[A-Z][a-z]{2,}", validLastName);
     }
+    public static boolean givenEmail_WhenValid_ShouldReturnTrue(String validUserMail) {
+        return Pattern.matches("^[a-z]+([_+.-]?[a-z0-9])*(@)(bl.co.)[a-z]{2}$", validUserMail);
+    }
     public static void main(String[] args) {
                 boolean firstName = givenFirstName_WhenValid_ShouldReturnTrue("Riya");
                 System.out.println("first name is valid" + firstName);
         boolean lastName = givenFirstName_WhenValid_ShouldReturnTrue("Meera");
         System.out.println("lastName is valid" + lastName);
+        boolean validUserMail = givenEmail_WhenValid_ShouldReturnTrue("abc.xyz@bl.co.in");
+        System.out.println("user mail id is valid" + validUserMail);
     }
 
 }
