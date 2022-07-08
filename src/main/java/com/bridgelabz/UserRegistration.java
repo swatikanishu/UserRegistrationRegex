1f9ce0ede233b881c8d974004c25b7055f3f429e
swatikanishu <nishuswatika@gmail.com>
2022-07-08 21:29:33 +0530

    Added: First name validation added

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,7 +1,13 @@
 package com.bridgelabz;
-
+import java.util.regex.Pattern;
 public class UserRegistration {
-    public static void main(String[] args) {
-        System.out.println(" welcome to UserRegistration program");
+    public static boolean givenFirstName_WhenValid_ShouldReturnTrue(String validFirstName) {
+        return Pattern.matches("^[A-Z][a-z]{2,}", validFirstName);
     }
+
+    public static void main(String[] args) {
+                boolean firstName = givenFirstName_WhenValid_ShouldReturnTrue("Riya");
+                System.out.println("first name is valid" + firstName);
+    }
+
 }
