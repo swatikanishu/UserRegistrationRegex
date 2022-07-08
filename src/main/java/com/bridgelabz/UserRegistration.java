53502f9bfef28285f8fb444bef05a841a22fe39c
swatikanishu <nishuswatika@gmail.com>
2022-07-08 21:42:24 +0530

    Added: Mobile number validation added

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -11,6 +11,10 @@ public class UserRegistration {
     public static boolean givenEmail_WhenValid_ShouldReturnTrue(String validUserMail) {
         return Pattern.matches("^[a-z]+([_+.-]?[a-z0-9])*(@)(bl.co.)[a-z]{2}$", validUserMail);
     }
+    public static boolean givenUserMobileNum_WhenValidFormat_ShouldReturnTrue(String validUserMobileNum) {
+        return Pattern.matches("^(91)[\\s][0-9]{10}", validUserMobileNum);
+    }
+
     public static void main(String[] args) {
                 boolean firstName = givenFirstName_WhenValid_ShouldReturnTrue("Riya");
                 System.out.println("first name is valid" + firstName);
@@ -18,6 +22,9 @@ public class UserRegistration {
         System.out.println("lastName is valid" + lastName);
         boolean validUserMail = givenEmail_WhenValid_ShouldReturnTrue("abc.xyz@bl.co.in");
         System.out.println("user mail id is valid" + validUserMail);
+        boolean validUserMobileNum = givenUserMobileNum_WhenValidFormat_ShouldReturnTrue("91 2345677892");
+        System.out.println("user mobile number is valid" + validUserMobileNum);
+
     }
 
 }
