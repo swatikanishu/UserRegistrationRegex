a88ceaa740bf5751a2944f306e9ab6fdb0f81dd8
swatikanishu <nishuswatika@gmail.com>
2022-07-11 00:24:07 +0530

    Added:Email validation added

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -17,6 +17,9 @@ public class UserRegistration {
     public static boolean givenPassword_WhenValid_ShouldReturnTrue(String validUserPassword) {
         return Pattern.matches("(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*#?&]{8,}$", validUserPassword);
     }
+    public static boolean givenSampleEmails_WhenValid_ShouldReturnTrue(String validEmailSample) {
+        return Pattern.matches("^(abc)([+-.]?[\\d])*[@][0-9a-z]+([.][a-z]{2,3}){1,2}$", validEmailSample);
+    }
 
 
     public static void main(String[] args) {
@@ -30,7 +33,23 @@ public class UserRegistration {
         System.out.println("user mobile number is valid" + validUserMobileNum);
         boolean validUserPassword = givenPassword_WhenValid_ShouldReturnTrue("wertP7$%");
         System.out.println("user password of minimum 8 char and at least 1 upper case,1 num,1 special char is valid" + validUserPassword);
+        boolean validSampleEmail1 = givenSampleEmails_WhenValid_ShouldReturnTrue("abc@yahoo.com");
+        System.out.println("email is valid" + validSampleEmail1);
+        boolean validSampleEmail2 = givenSampleEmails_WhenValid_ShouldReturnTrue("abc-100@yahoo.com");
+        System.out.println("email is valid" + validSampleEmail2);
+        boolean validSampleEmail3 = givenSampleEmails_WhenValid_ShouldReturnTrue("abc.100@yahoo.com");
+        System.out.println("email is valid" + validSampleEmail3);
+        boolean validSampleEmail4 = givenSampleEmails_WhenValid_ShouldReturnTrue("abc111@abc.com");
+        System.out.println("email is valid" + validSampleEmail4);
+        boolean validSampleEmail5 = givenSampleEmails_WhenValid_ShouldReturnTrue("abc-100@abc.net");
+        System.out.println("email is valid" + validSampleEmail5);
+        boolean validSampleEmail6 = givenSampleEmails_WhenValid_ShouldReturnTrue("abc.100@abc.com.au");
+        System.out.println("email is valid" + validSampleEmail6);
+        boolean validSampleEmail7 = givenSampleEmails_WhenValid_ShouldReturnTrue("abc@1.com");
+        System.out.println("email is valid" + validSampleEmail7);
+        boolean validSampleEmail8 = givenSampleEmails_WhenValid_ShouldReturnTrue("abc@gmail.com.com");
+        System.out.println("email is valid" + validSampleEmail8);
+    }
+}
 
-    }
-    }
 
